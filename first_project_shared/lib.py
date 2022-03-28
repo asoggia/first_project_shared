d013bae86cc7f32c86c531bce4ac17f4bb1c5ee4
Andrea Soggia <andrea.soggia@gmail.com>
2022-03-28 14:15:02 +0200

    Initial Push

---
+++ first_project_shared/lib.py
@@ -0,0 +1,8 @@
+def try_me():
+    print(
+        "Hello Diana, j'espère que tu passes une bonne journée malgré cette matinée galère avec les retards de train"
+    )
+
+if __name__ == "__main__":
+    result = try_me()
+    return result
